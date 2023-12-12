6103a74210491c8fe184f9c52f30034b2dae28f3
sayedgallaf <sayedgallaf@gmail.com>
2023-12-12 12:32:53 +0300

    Listings page functionality and backend, with jobModel, jobController and much more

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -81,7 +81,7 @@ const UserController = {
               httpOnly: true,
               secure: false
             });
-
+            console.log("test")
             res.status(201).json({ message: 'User created successfully', userId: createdUserId });
         } catch (error) {
             res.status(500).json({ message: `Error: ${error.message}` });
