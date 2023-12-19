9390df4fb508261f10a8b8fbf2f1e2b1ee737a32
sayedgallaf <sayedgallaf@gmail.com>
2023-12-19 13:46:12 +0300

    commit

--- app/config/createTables.js
+++ app/config/createTables.js
@@ -4,10 +4,10 @@ const bcrypt = require("bcrypt");
 (async () => {
 /*     console.log(await User.getAllUsers())
     return; */
-    await db.query(`DROP DATABASE freelance_dolphin`);
+/*     await db.query(`DROP DATABASE freelance_dolphin`);
     await db.query(`CREATE DATABASE freelance_dolphin`);
     console.log("DONE")
-    return;
+    return; */
     const sqlStatements = [
         `CREATE TABLE User (
           UserID VARCHAR(20) PRIMARY KEY,
