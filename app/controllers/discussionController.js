c4b9636674dd814451f838483d7ed418718cf79c
sayedgallaf <sayedgallaf@gmail.com>
2023-12-30 21:19:20 +0300

    commit

--- app/controllers/discussionController.js
+++ app/controllers/discussionController.js
@@ -84,7 +84,6 @@ const discussionController = {
 
                 const freelancer = await User.getUserById(UserID);
                 const employer = await User.getUserById(job.UserID);
-                console.log(freelancer,employer)
                 resend.emails.send({
                     from: 'support@dolphin.directory',
                     to: freelancer.Email,
